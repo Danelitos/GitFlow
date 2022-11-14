3033b1cda0f72bd32c6cac70b6fa6ff04fe3a4a9
Danelitos <danel.alonso10@gmail.com>
2022-11-14 13:20:21 +0100

    Instrucciones de como hacer un commit mediante GitFlow

--- GitFlow.sh
+++ GitFlow.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-echo Cúal es la rama que quieres crear y hacer modificaciones?
+RESET="\e[0m"
+RED="\e[31m"
+BLUE="\e[34m"
+echo $RED Cúal es la rama que quieres crear y hacer modificaciones? $RESET
 read rama
-echo $rama
-echo Estas son las ramas que tienes y la que tiene el * es en la que estas actualmente
+echo "\n" $RED Estas son las ramas que tienes y la que tiene el * es en la que estas actualmente $RESET "\n"
 git branch
-echo Creamos la rama $rama y nos movemos a ella
+echo $RED Creamos la rama $rama y nos movemos a ella $RESET "\n"
 git checkout -b $rama
-echo Añadimos los cambios que hayamos hecho en nuestro proyecto
+echo $RED Añadimos los cambios que hayamos hecho en nuestro proyecto $RESET "\n"
 git add .
-echo Hacemos commit y damos información de lo que hemos modificado
+echo $RED Hacemos commit y damos información de lo que hemos modificado $RESET "\n"
 git commit
 git push origin $rama
-echo Nos movemos a la rama de trabajo, cúal es tu rama de trabajo?
+echo $RED Nos movemos a la rama de trabajo, cúal es tu rama de trabajo? $RESET
 read ramaTrabajo
 git checkout $ramaTrabajo
-echo Hacemos merge de $rama a develop
+echo "\n" $RED Hacemos merge de $rama a develop $RESET "\n"
 git merge --no-ff $rama
 git push origin $ramaTrabajo
-echo Finalmente borramos $rama y luego también hay que hacerlo de forma física en gitHub
+echo $RED Finalmente borramos $rama y luego también hay que hacerlo de forma física en gitHub $RESET "\n"
 git branch -d $rama
